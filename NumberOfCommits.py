540da8b5088e3737d2069667ffb6a1f46b0e3b77
dingtongwang0 <dingtongwang@outlook.com>
2019-09-25 02:16:06 -0400

    update NumberOfCommit.py

--- NumberOfCommits.py
+++ NumberOfCommits.py
@@ -10,10 +10,16 @@ def get_repo_list(github_id):
 
 
 def get_com_num(github_id, repo_name):
-    response = requests.get(f"https://api.github.com/repos/{github_id}/{repo_name}/commits")
-    return len(response.json())
+    response = requests.get(f"https://api.github.com/repos/{github_id}/{repo_name}/stats/contributors")
+    if response.status_code == 200:
+        count = 0
+        for contributor_info in response.json():
+            count = count + int(contributor_info['total'])
+        return count
+    else:
+        return "NA"
 
 
 def main(github_id):
-    for repository in get_repo_list(github_id):
-        print(f"{repository}: {get_com_num(github_id, repository)}")
+    for repository_name in get_repo_list(github_id):
+        print(f"{repository_name}: {get_com_num(github_id, repository_name)}")
